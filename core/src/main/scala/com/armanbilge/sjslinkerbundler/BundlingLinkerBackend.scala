baf00b2f53057125be44c7bd18ba4fd943a16540
Arman Bilge <armanbilge@gmail.com>
2022-09-07 21:55:03 +0000

    Begin interacting with closure compiler

--- core/src/main/scala/com/armanbilge/sjslinkerbundler/BundlingLinkerBackend.scala
+++ core/src/main/scala/com/armanbilge/sjslinkerbundler/BundlingLinkerBackend.scala
@@ -16,6 +16,11 @@
 
 package com.armanbilge.sjslinkerbundler
 
+import com.google.javascript.jscomp.Compiler
+import com.google.javascript.jscomp.CompilerOptions
+import com.google.javascript.jscomp.JSChunk
+import com.google.javascript.jscomp.SourceFile
+import org.scalajs.linker.MemOutputDirectory
 import org.scalajs.linker.interface.OutputDirectory
 import org.scalajs.linker.interface.Report
 import org.scalajs.linker.interface.StandardConfig
@@ -24,22 +29,76 @@ import org.scalajs.linker.standard.ModuleSet
 import org.scalajs.linker.standard.StandardLinkerBackend
 import org.scalajs.logging.Logger
 
+import java.io.ByteArrayInputStream
+import java.util.Arrays
+import java.util.Collections
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
-final class BundlingLinkerBackend(linkerConfig: StandardConfig) extends LinkerBackend {
+final class BundlingLinkerBackend(
+    linkerConfig: StandardConfig,
+    compilerOptions: CompilerOptions
+) extends LinkerBackend {
+
+  compilerOptions.setChunkOutputType(CompilerOptions.ChunkOutputType.ES_MODULES)
+
   private[this] val standard = StandardLinkerBackend(linkerConfig)
 
-  override val coreSpec = standard.coreSpec
+  val coreSpec = standard.coreSpec
 
-  override val symbolRequirements = standard.symbolRequirements
+  val symbolRequirements = standard.symbolRequirements
 
-  override def injectedIRFiles = standard.injectedIRFiles
+  def injectedIRFiles = standard.injectedIRFiles
 
-  override def emit(moduleSet: ModuleSet, output: OutputDirectory, logger: Logger)(
+  def emit(moduleSet: ModuleSet, output: OutputDirectory, logger: Logger)(
       implicit ec: ExecutionContext
   ): Future[Report] = {
-    standard.emit(moduleSet, output, logger)
+    val memOutput = MemOutputDirectory()
+    standard.emit(moduleSet, memOutput, logger).map { report =>
+      val publicModules =
+        report.publicModules.toList.flatMap(m => memOutput.content(m.jsFileName).map(m -> _))
+
+      val publicModuleNames = report.publicModules.map(_.jsFileName).toSet
+      val internalFiles = memOutput
+        .fileNames()
+        .filterNot(publicModuleNames.contains(_))
+        .flatMap(fn => memOutput.content(fn).map(fn -> _))
+
+      val internalChunk = new JSChunk("internal")
+      internalFiles.foreach {
+        case (fn, content) =>
+          internalChunk.add(
+            SourceFile
+              .builder()
+              .withPath(fn)
+              .withContent(new ByteArrayInputStream(content))
+              .build()
+          )
+      }
+
+      val publicChunks = publicModules.map {
+        case (mod, content) =>
+          val ch = new JSChunk(mod.moduleID)
+          ch.addDependency(internalChunk)
+          ch.add(
+            SourceFile
+              .builder()
+              .withPath(mod.jsFileName)
+              .withContent(new ByteArrayInputStream(content))
+              .build()
+          )
+          ch
+      }
+
+      val compiler = new Compiler
+      compiler.compileModules(
+        Collections.emptyList,
+        Arrays.asList((publicChunks :+ internalChunk): _*),
+        compilerOptions
+      )
+
+      report
+    }
   }
 
 }
